659863fc9d44a296008432c98f3a7b1e5f97cee9
23520641 <23520641@gm.uit.edu.vn>
2024-06-03 21:42:19 +0700

    Dinh nghia tro choi Snake

---
+++ dinhnghiagame.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <winbgim.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+#define 	MINW 		20
+#define 	MINH 		20
+#define 	MAXW 		420
+#define 	MAXH 		220
+#define 	MINX 		20
+#define 	MINY 		20
+#define 	MAXX 		420
+#define 	MAXY 		220
+#define 	BACKGROUND	15
+
