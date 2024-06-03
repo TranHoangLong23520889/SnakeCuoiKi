ef6fc9536c4c7de1869a7f59d5e7016a614cb6d3
TranHoangLong23520889 <23520889@gm.uit.edu.vn>
2024-06-03 19:15:04 +0700

    kiem tra file luu diem cao

---
+++ isEmpty.cpp
@@ -0,0 +1,11 @@
+bool isEmpty(){
+	FILE *fp;
+	long size;
+	fp = fopen("highscore.txt","r");
+	if(fp){
+		fseek (fp, 0, SEEK_END);
+        size = ftell(fp);
+		fclose(fp);
+	}
+	return(size == 0);
+}
